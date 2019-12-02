205408f61187ae97d291ef96e6fc35736cdbc35c
matija <matijakolaric@users.noreply.github.com>
2019-12-02 14:25:07 +0100

    Streaming JSON response

--- edi/views.py
+++ edi/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import StreamingHttpResponse
 from django import forms
 from music_metadata.edi.file import EdiFile
 from django.conf import settings
+from django.core.serializers.json import DjangoJSONEncoder
 try:
     # not part of open source yet
     # without it, just basic EDI processing is available
@@ -10,6 +11,7 @@ try:
 except:
     raise
 from django.views import View
+from collections import OrderedDict
 import json
 
 
@@ -47,6 +49,30 @@ class ToJson(View):
             'title': 'EDI to JSON conversion',
             'form': form})
 
+    @staticmethod
+    def to_json(edi_file):
+        yield '{"work_registrations": [\n'
+        errors = []
+        for i, group in enumerate(edi_file.get_groups()):
+            if i:
+                yield ', '
+            for j, transaction in enumerate (group.get_transactions()):
+                if j:
+                    yield ', '
+                yield json.dumps(
+                    transaction.to_dict(), cls=DjangoJSONEncoder, indent=4)
+            errors += group.errors
+        yield '],\n"file": '
+        file_data = OrderedDict()
+        file_data['name'] = edi_file.name
+        file_data['valid'] = edi_file.valid
+        file_data['work_count'] = edi_file.transaction_count
+        file_data['line_count'] = edi_file.record_count
+        file_data['errors'] = [str(e) for e in (edi_file.file_errors + errors)]
+        yield json.dumps(file_data, indent=4)
+        yield '}'
+
+
     def post(self, request, *args, **kwargs):
         form = FileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -54,11 +80,9 @@ class ToJson(View):
             edi_file = Cwr2File(f)
             edi_file.seek(0)
             edi_file.reconfigure(encoding='latin1')
-            d = edi_file.to_dict()
-            response = JsonResponse(
-                d,
-                json_dumps_params={'indent': 4},
-            )
+            d = self.to_json(edi_file)
+            response = StreamingHttpResponse(d)
+            response['Content-Type'] = 'application/json'
             response['Content-Disposition'] = f'attachment; filename="{ edi_file.name }.json"'
             return response
         else:
